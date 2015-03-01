37679911730845a00dfb95a698abd48e73d6b5f5
Steve Singh <steve.a.singh@gmail.com>
2015-02-28 22:35:18 -0500

    Fixed javascript issue that was preventing proper floating label functionality with forms.

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,5 +1,4 @@
 +function ($) {
-  'use strict';
 
   function setInputValue() {
     var val = $(this).val();
@@ -19,9 +18,8 @@
     var isFirefox = typeof InstallTrigger !== 'undefined';
     var isIE = /*@cc_on!@*/false || !!document.documentMode;
     if (isFirefox || isIE) {
-      $(".input input[type='text']:not([value]), .input input[type='email']:not([value]), .input input[type='password']:not([value])")
-          .filter(function() { return $(this).val();}).refresh();
+      $(".input input[type='text']:not([value]), .input input[type='email']:not([value]), .input input[type='password']:not([value])").filter(function() { return $(this).val();}).refresh();
     }
   });
 
-}(Zepto || jQuery || $);
+}(jQuery);
